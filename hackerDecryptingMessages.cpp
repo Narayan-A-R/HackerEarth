bd9e0c19b78b6f4137e8c60b900a938857131ae2
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-14 17:41:10 +0530

    online solution

--- hackerDecryptingMessages.cpp
+++ hackerDecryptingMessages.cpp
@@ -1,147 +1,86 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
 const int N=1e6+10;
-int a[N]={0};
-// int hp[N]={0};
-// int lp[N]={0};
-vector<int> isPrime(N,0);
-vector<int> v;
-vector<vector<int>> allPrimeFactors(N,v);
-int isPowerOfArr[N]={0};
+int hp[N]={0};
+int canRemove[N]={0};
+int hsh[N]={0};
+
+vector<int> distinctPF(int x){
+    vector<int> ans;
+    while (x>1){
+        int pf=hp[x];
+        while(x%pf==0){
+            x/=pf;
+        }
+        ans.push_back(pf);
+    }
+    return ans;
+}
 
 int main(){
+    for (int i = 2; i < N; i++){
+        if (hp[i]==0){
+            for (int  j = i; j < N; j+=i){
+                hp[j]=i;
+            }
+        }
+    }
+    
     int n,q;
     cin>>n>>q;
-    for (int i = 0; i <n; i++)
-    {
-        cin>>a[i];
+    for (int i = 0; i <n; i++){
+        int x;
+        cin>>x;
+        hsh[x]=1;
     }
-    
-    for (int i = 0; i < n; i++)
+    for (int i = 2; i <N; i++)
     {
-        for (long long int j = a[i]; j < N; j*=j)
+        if (hsh[i])
         {
-            isPowerOfArr[j]=1;
-        }
-    }
-
-    
-    for (int i = 2; i < N; i++)
-    {
-        if (isPrime[i] == 0)
-        {
-            for (int j = i; j < N; j+=i)
+            for (long long int j = i; j < N; j*=i)
             {
-                allPrimeFactors[j].push_back(i);
-                isPrime[j]++;
-                // hp[j]=i;
-                // if (lp[j]==0)
-                // {
-                //     lp[j]==i;
-                // }
-                
+                canRemove[j]=1;
             }
         }
     }
-
-    unordered_map<int,string> xtoAns;
-    for (int x = 2; x < N; x++)
-    {
-        int found=0;
-        if (isPrime[x])
-        {
-            xtoAns[x]="NO\n";
-        }
-        
-        for (auto it = allPrimeFactors[x].begin(); it != allPrimeFactors[x].end(); it++)
-        {
-            if (found)
-            {
-                break;
-            }
-            for (auto itr=it; itr!=allPrimeFactors[x].end(); itr++)
-            {
-                int ans=x/((*it)*(*itr));
-                if (found)
-                {
-                    break;
-                }
-                
-                if (*it == *itr && (x/(*it))%(*it)!=0)
-                {
-                    xtoAns[x]="NO\n";
-                    continue;
-                }
-                
-                if (isPowerOfArr[ans] )
-                {
-                    xtoAns[x]="YES\n";
-                    found=1;
-                }
-                else{
-                    xtoAns[x]="NO\n";
-                }
-            }
-        }
-        
-    }
-    
-
-    
-    // for(auto value:allPrimeFactors[429]){
-    //     cout<<value<<" ";
-    // }
-
-    // for (size_t i = 0; i < 430; i++)
-    // {
-    //     cout<<isPowerOfArr[i]<<" "<<i<<" \n";
-    // }
-    
-    // for (auto it = allPrimeFactors[42].begin(); it != allPrimeFactors[42].end(); it++)
-    // {
-    //     for (auto itr=it; itr!=allPrimeFactors[42].end(); itr++)
-    //     {
-    //         int ans=42/((*it)*(*itr));
-    //         cout<<ans<<" ans\n";
-    //         cout<<*it<<" --"<<*itr<<" ++\n";
-    //         if (*it == *itr && (42/(*it))%(*it)!=0)
-    //         {
-    //             xtoAns[42]="NO\n";
-    //             int sol=(42/(*it))%(*it)!=0;
-    //             cout<<"----------"<<sol<<" -------exp\n\n\n";
-    //             continue;
-    //         }
-            
-
-    //         // cout<<sol<<" sol\n\n";
-    //         if (isPowerOfArr[ans])
-    //         {
-    //             xtoAns[42]="YES\n";
-    //         }
-    //         else{
-    //             xtoAns[42]="NO\n";
-    //         }
-    //     }
-    // }
     while (q--)
     {
         int x;
         cin>>x;
-        cout<<xtoAns[x];
-
+        vector<int> pf=distinctPF(x);
+        bool isPossible=false;
+        for (int i = 0; i < pf.size(); i++)
+        {
+            for (int j = i; j < pf.size(); j++)
+            {
+                int product=pf[i]*pf[j];
+                if (i==j && x%product!=0)
+                {
+                    continue;
+                }
+                int toRemove=x/product;
+                if (canRemove[toRemove]==1 || toRemove==1)
+                {
+                    isPossible=true;
+                    break;
+                }
+                
+            }
+            if (isPossible)
+            {
+                break;
+            }
+        }
+        if (isPossible)
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
     }
 
-// 
-// 79 7
-// 72 11 72 59 100 21 23 87 88 20 12 52 88 52 52 18 15 27 27 78 56 80 86 94 46 100 76 2 86 60 13 54 74 85 27 63 66 92 97 70 82 15 92 2 100 38 59 51 94 73 29 20 42 49 87 76 64 37 81 27 65 61 41 32 60 74 38 47 93 32 75 17 58 65 55 4 10 63 51
-// 474
-// 168
-// 28
-// 476
-// 908
-// 2
-// 4  ans is supposed to be yes, 2 x 2 = 4, if we divide 4 by 1 we get it as multiple of 2 primes , but no element 1 in array
+
     return 0;
 }   
