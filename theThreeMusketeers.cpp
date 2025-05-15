f729a3c047913b3b1aff8365a2827679f33a05d7
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-15 21:45:40 +0530

    Input

---
+++ theThreeMusketeers.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+const int N=1e4+10;
+string names[N];
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        for (int i = 0; i <n; i++)
+        {
+            cin>>names[i];
+        }
+        
+    }
+    
+    return 0;
+}
