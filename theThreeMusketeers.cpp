5f440fe66b6105066560c0b130ddb0107ba9f0fd
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-16 11:42:42 +0530

    Hidden test case passed

--- theThreeMusketeers.cpp
+++ theThreeMusketeers.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
+#include<bitset>
 using namespace std;
 const int N=1e4+10;
 int vowelInNames[N];
-int choose(int n){
+long long int choose(int n){
     if (n<3) return 0;
-
-    int m=1;
+    long long int m=1;
     for (int i = n; i >n-3 && n >=3; i--)
     {
         m*=i;
@@ -13,8 +13,8 @@ int choose(int n){
     return m/6;
 
 }
-int common1(int * vowelInNames,int capacity){
-    int sum=0;
+long long int common1(int * vowelInNames,int capacity){
+    long long int sum=0;
     for (int j = 0; j < 5; j++)
     {
         int cnt=0;
@@ -26,24 +26,24 @@ int common1(int * vowelInNames,int capacity){
     }
     return sum;
 }
-int common2(int * vowelInNames,int capacity){
-    int sum=0;
+long long int common2(int * vowelInNames,int capacity){
+    long long int sum=0;
     for (int k = 0; k < 4; k++)
     {
         for (int j = k+1; j < 5; j++)
         {
             int cnt=0;
-            for (int i = 0; i < capacity-1; i++)
+            for (int i = 0; i < capacity; i++)
             {
-                if(vowelInNames[i]&(1<<j) && vowelInNames[i+1]&(1<<k)) cnt++;
+                if(vowelInNames[i]&(1<<j) && vowelInNames[i]&(1<<k)) cnt++;
             }
             sum+=choose(cnt);
         }
     }
     return sum;
 }
-int common3(int * vowelInNames,int capacity){
-    int sum=0;
+long long int common3(int * vowelInNames,int capacity){
+    long long int sum=0;
     for (int l = 0; l < 3; l++)
     {
         for (int k = l+1; k < 4; k++)
@@ -61,8 +61,8 @@ int common3(int * vowelInNames,int capacity){
     }
     return sum;
 }
-int common4(int * vowelInNames,int capacity){
-    int sum=0;
+long long int common4(int * vowelInNames,int capacity){
+    long long int sum=0;
     for (int m = 0; m < 2; m++)
     {
         for (int l = m+1; l < 3; l++)
@@ -83,9 +83,8 @@ int common4(int * vowelInNames,int capacity){
     }
     return sum;
 }
-
-int common5(int * vowelInNames,int capacity){
-    int sum=0;
+long long int common5(int * vowelInNames,int capacity){
+    long long int sum=0;
     for (int n = 0; n < 1; n++)
     {
         for (int m = n+1; m < 2; m++)
@@ -139,10 +138,11 @@ int main(){
             }
             vowelInNames[i]=vowelsInbits;
         }
+        
+        
 
 
-
-        int common=  common1(vowelInNames,n)
+        long long int common=  common1(vowelInNames,n)
                     -common2(vowelInNames,n)
                     +common3(vowelInNames,n)
                     -common4(vowelInNames,n)
