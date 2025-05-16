4e2afec3c830b354cea4f0ff526895a547b91567
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-16 10:51:04 +0530

    Test case passed, hidden cases wrong ans

--- theThreeMusketeers.cpp
+++ theThreeMusketeers.cpp
@@ -1,52 +1,68 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 const int N=1e4+10;
 int vowelInNames[N];
-int common1(int * vowelInNames,int N){
-    int cnt=0;
+int choose(int n){
+    if (n<3) return 0;
+
+    int m=1;
+    for (int i = n; i >n-3 && n >=3; i--)
+    {
+        m*=i;
+    }
+    return m/6;
+
+}
+int common1(int * vowelInNames,int capacity){
+    int sum=0;
     for (int j = 0; j < 5; j++)
     {
-        for (int i = 0; i < N; i++)
+        int cnt=0;
+        for (int i = 0; i < capacity; i++)
         {
-            if(vowelInNames[i]&&(1<<j)) cnt++;
+            if(vowelInNames[i]&(1<<j)) cnt++;
         }
+        sum+=choose(cnt);
     }
-    return cnt;
+    return sum;
 }
-int common2(int * vowelInNames,int N){
-    int cnt=0;
+int common2(int * vowelInNames,int capacity){
+    int sum=0;
     for (int k = 0; k < 4; k++)
     {
         for (int j = k+1; j < 5; j++)
         {
-            for (int i = 0; i < N; i++)
+            int cnt=0;
+            for (int i = 0; i < capacity-1; i++)
             {
-                if(vowelInNames[i]&&(1<<j) && vowelInNames[i]&&(1<<k)) cnt++;
+                if(vowelInNames[i]&(1<<j) && vowelInNames[i+1]&(1<<k)) cnt++;
             }
+            sum+=choose(cnt);
         }
     }
-    return cnt;
+    return sum;
 }
-int common3(int * vowelInNames,int N){
-    int cnt=0;
+int common3(int * vowelInNames,int capacity){
+    int sum=0;
     for (int l = 0; l < 3; l++)
     {
         for (int k = l+1; k < 4; k++)
         {
             for (int j = k+1; j < 5; j++)
             {
-                for (int i = 0; i < N; i++)
+                int cnt=0;
+                for (int i = 0; i < capacity; i++)
                 {
-                    if(vowelInNames[i]&&(1<<j) && vowelInNames[i]&&(1<<k)&& vowelInNames[i]&&(1<<l)) cnt++;
+                    if(vowelInNames[i]&(1<<j) && vowelInNames[i]&(1<<k) && vowelInNames[i]&(1<<l)) cnt++;
                 }
+                sum+=choose(cnt);
             }
         }
     }
-    return cnt;
+    return sum;
 }
-int common4(int * vowelInNames,int N){
-    int cnt=0;
+int common4(int * vowelInNames,int capacity){
+    int sum=0;
     for (int m = 0; m < 2; m++)
     {
         for (int l = m+1; l < 3; l++)
@@ -55,19 +71,21 @@ int common4(int * vowelInNames,int N){
             {
                 for (int j = k+1; j < 5; j++)
                 {
-                    for (int i = 0; i < N; i++)
+                    int cnt=0;
+                    for (int i = 0; i < capacity; i++)
                     {
-                        if(vowelInNames[i]&&(1<<j) && vowelInNames[i]&&(1<<k)&& vowelInNames[i]&&(1<<l) && vowelInNames[i]&&(1<<m)) cnt++;
+                        if(vowelInNames[i]&(1<<j) && vowelInNames[i]&(1<<k) && vowelInNames[i]&(1<<l) && vowelInNames[i]&(1<<m)) cnt++;
                     }
+                    sum+=choose(cnt);
                 }
             }
         }
     }
-    return cnt;
+    return sum;
 }
 
-int common5(int * vowelInNames,int N){
-    int cnt=0;
+int common5(int * vowelInNames,int capacity){
+    int sum=0;
     for (int n = 0; n < 1; n++)
     {
         for (int m = n+1; m < 2; m++)
@@ -78,10 +96,12 @@ int common5(int * vowelInNames,int N){
                 {
                     for (int j = k+1; j < 5; j++)
                     {
-                        for (int i = 0; i < N; i++)
+                        int cnt=0;
+                        for (int i = 0; i < capacity; i++)
                         {
-                            if(vowelInNames[i]&&(1<<j) && vowelInNames[i]&&(1<<k)&& vowelInNames[i]&&(1<<l) && vowelInNames[i]&&(1<<m) && vowelInNames[i]&&(1<<n)) cnt++;
+                            if(vowelInNames[i]&(1<<j) && vowelInNames[i]&(1<<k) && vowelInNames[i]&(1<<l) && vowelInNames[i]&(1<<m) && vowelInNames[i]&(1<<n)) cnt++;
                         }
+                        sum+=choose(cnt);
                         
                     }
                     
@@ -92,8 +112,10 @@ int common5(int * vowelInNames,int N){
         }
         
     }
-    return cnt;
+    return sum;
 }
+
+
 int main(){
     int t;
     cin>>t;
@@ -109,38 +131,33 @@ int main(){
             int vowelsInbits=0;
             for (int j = 0; j < str.size(); j++)
             {
-                if (str[i]== 'a') vowelsInbits|(1<<4);
-                if (str[i]== 'e') vowelsInbits|(1<<3);
-                if (str[i]== 'i') vowelsInbits|(1<<2);
-                if (str[i]== 'o') vowelsInbits|(1<<1);
-                if (str[i]== 'u') vowelsInbits|(1<<0);
+                if (str[j]== 'a') vowelsInbits|=(1<<4);
+                if (str[j]== 'e') vowelsInbits|=(1<<3);
+                if (str[j]== 'i') vowelsInbits|=(1<<2);
+                if (str[j]== 'o') vowelsInbits|=(1<<1);
+                if (str[j]== 'u') vowelsInbits|=(1<<0);
             }
             vowelInNames[i]=vowelsInbits;
         }
 
-        
-        int common=common1(vowelInNames,n)-common2(vowelInNames,n)+common3(vowelInNames,n)-common4(vowelInNames,n)+common5(vowelInNames,n);
 
-        if (common>=3)
-        {
-            int m=1;
-            for (int i = common; i >common-3 && common >=3; i--)
-            {
-                m*=i;
-            }
-            int ans=m/6;
-            cout<<ans<<'\n';
-        }
-        else
-        {
-            cout<<0<<'\n';
-        }
+
+        int common=  common1(vowelInNames,n)
+                    -common2(vowelInNames,n)
+                    +common3(vowelInNames,n)
+                    -common4(vowelInNames,n)
+                    +common5(vowelInNames,n);
+
+        // cout<<common1(vowelInNames,n)<<"  --- 1\n";
+        // cout<<common2(vowelInNames,n)<<"  --- 2\n";
+        // cout<<common3(vowelInNames,n)<<"  --- 3\n";
+        // cout<<common4(vowelInNames,n)<<"  --- 4\n";
+        // cout<<common5(vowelInNames,n)<<"  --- 5\n";
+        // cout<<common<<"  ---------------\n";
+        cout<<common<<'\n';
         
         
-        // if( vowelsInNames[k]&(1<<i) && && vowelInNames[k]&(1<<j) && vowelInNames[l]&(1<<j))
-        // either a is common or e is common or i is common or o is common or u is common
-        //  a b c
-        // 
+        
     }
     
     return 0;
